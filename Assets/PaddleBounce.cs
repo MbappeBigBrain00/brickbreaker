0e9a505844a69ea6cbefa1306e60bb3c84208aa9
MbappeBigBrain00 <skibidimbappemcfriedchicken@outlook.com>
2025-11-13 20:17:48 +0700

    intro

---
+++ Assets/PaddleBounce.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Paddle : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    public float speed = 30f;
+    public float maxBounceAngle = 75f;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ball")) {
+            return;
+        }
+
+        Rigidbody2D ball = collision.rigidbody;
+        Collider2D paddle = collision.otherCollider;
+
+        // Gather information about the collision
+        Vector2 ballDirection = ball.velocity.normalized;
+        Vector2 contactDistance = paddle.bounds.center - ball.transform.position;
+
+        float bounceAngle = (contactDistance.x / paddle.bounds.size.x) * maxBounceAngle;
+        ballDirection = Quaternion.AngleAxis(bounceAngle, Vector3.forward) * ballDirection;
+
+        // Re-apply the new direction to the ball
+        ball.velocity = ballDirection * ball.velocity.magnitude;
+    }
+}
